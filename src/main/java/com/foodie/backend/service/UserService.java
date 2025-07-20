e0a8551e0a708bebd81f07b6aa7e4cc8653866f7
syedfawwaz17 <syedfawwaz17@gmail.com>
2025-07-20 13:00:11 +0530

    User Service

--- src/main/java/com/foodie/backend/service/UserService.java
+++ src/main/java/com/foodie/backend/service/UserService.java
@@ -5,9 +5,12 @@ import com.foodie.backend.dto.UserDTO;
 import java.util.List;
 
 public interface UserService {
-    UserDTO createUser(UserDTO userDto);
+    UserDTO registerUser(UserDTO userDto, String rawPassword);
+
     UserDTO getUserById(String id);
     List<UserDTO> getAllUsers();
     UserDTO updateUser(String id, UserDTO userDto);
     void deleteUser(String id);
+    UserDTO findByEmail(String email);
+    boolean existsByEmail(String email);
 }
--- src/main/java/com/foodie/backend/service/UserServiceImpl.java
+++ src/main/java/com/foodie/backend/service/UserServiceImpl.java
@@ -1,13 +1,84 @@
 package com.foodie.backend.service;
 
+import com.foodie.backend.dto.UserDTO;
+import com.foodie.backend.mapper.UserMapper;
+import com.foodie.backend.model.User;
 import com.foodie.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService{
     @Autowired
     private UserRepository userRepository;
     @Autowired
-    private UserMapp
+    private UserMapper userMapper;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Override
+    public UserDTO registerUser(UserDTO userDTO, String rawPassword){
+        if(userRepository.existsByEmail(userDTO.getEmail())){
+            throw new RuntimeException("Email already in use");
+        }
+        User user = userMapper.toEntity(userDTO);
+        user.setPassword(passwordEncoder.encode(rawPassword));
+        user = userRepository.save(user);
+        return userMapper.toDto(user);
+    }
+
+    @Override
+    public UserDTO getUserById(String id) {
+        User user = userRepository.findById(id).
+                orElseThrow(()->new RuntimeException("User Not Found"));
+
+        return userMapper.toDto(user);
+    }
+
+    @Override
+    public List<UserDTO> getAllUsers() {
+        List<User> all = userRepository.findAll();
+        return all.stream().
+                map(userMapper::toDto)
+                .toList();
+
+    }
+
+    @Override
+    public UserDTO updateUser(String id, UserDTO userDto) {
+
+        User found = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        found.setName(userDto.getName());
+        found.setEmail(userDto.getEmail());
+        User save = userRepository.save(found);
+        return userMapper.toDto(save);
+    }
+
+    @Override
+    public void deleteUser(String id) {
+
+         userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        userRepository.deleteById(id);
+
+
+
+    }
+
+    @Override
+    public UserDTO findByEmail(String email) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(()->new RuntimeException("User Not Found"));
+        return userMapper.toDto(user);
+    }
+
+    @Override
+    public boolean existsByEmail(String email) {
+        return userRepository.existsByEmail(email);
+    }
 }
